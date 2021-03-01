6cff69b9625373327050d157dcd198d3f4c0dc1f
Mica <mika89700@gmail.com>
2021-03-01 16:23:04 +0100

    Create start_qiskit.py

---
+++ data/start_qiskit.py
@@ -0,0 +1,30 @@
+import numpy as np
+from qiskit import QuantumCircuit, execute, Aer
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
+# Use Aer's qasm_simulator
+simulator = Aer.get_backend('qasm_simulator')
+
+circuit = QuantumCircuit(2, 2)
+
+# Add a H gate on qubit 0
+circuit.h(0)
+circuit.cx(0, 1)
+
+# Map the quantum measurement to the classical bits
+circuit.measure([0,1], [0,1])
+
+# Execute the circuit on the qasm simulator
+job = execute(circuit, simulator, shots=1000)
+
+# Grab results from the job
+result = job.result()
+
+# Returns counts
+counts = result.get_counts(circuit)
+print("\nTotal count for 00 and 11 are:",counts)
+
+# Draw the circuit
+circuit.draw(output='mpl')
+plt.show()
