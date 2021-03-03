cf59320a58f64dd12e5f4ced94abcc83f13adb34
Mica <mika89700@gmail.com>
2021-03-03 14:53:09 +0100

    Create config_git.sh

---
+++ build/config_git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# If you want to use and configure git, fullfill the next lines with your GitHub/GitLab information
+git config --global user.email « mail »
+git config --global user.name « username »
