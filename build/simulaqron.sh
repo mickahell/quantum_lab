2a394cca58e026f28f87db10ea5579cba601d6de
Mica <mika89700@gmail.com>
2021-04-01 11:36:15 +0200

    Update simulaqron.sh

--- build/simulaqron.sh
+++ build/simulaqron.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 pip3 install simulaqron
+
+# Getting last version of CQC-Python for quantum network examples
+apt-get update
+apt-get install git
+
+cd /opt/quantum_lab/data/simulaqron
+git clone https://github.com/SoftwareQuTech/CQC-Python.git
