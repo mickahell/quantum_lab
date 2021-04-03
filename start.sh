399e535884b4e1948b4428a0ba4d63e968f1f292
Mica <mika89700@gmail.com>
2021-04-03 10:30:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -v $(pwd)/data:/opt/quantum_lab/data/share --entrypoint=/bin/bash -p 8888:8888 -e LANG=C.UTF-8 quantum_lab
+docker run -it -v $(pwd)/data/share:/opt/quantum_lab/data/share --entrypoint=/bin/bash -p 8888:8888 -e LANG=C.UTF-8 quantum_lab
