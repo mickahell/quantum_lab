919a03dbcd289e6104f125ab75fdd19db1329f83
Michaël <mika89700@gmail.com>
2023-03-10 13:01:50 +0100

    Update qsharp.sh

--- build/qsharp.sh
+++ build/qsharp.sh
@@ -5,12 +5,13 @@ wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.
 dpkg -i packages-microsoft-prod.deb
 apt-get update
 apt-get install -y apt-transport-https
-apt-get install -y dotnet-sdk-3.1
-apt-get install -y aspnetcore-runtime-3.1
+apt-get install -y dotnet-sdk-6.0
+apt-get install -y aspnetcore-runtime-6.0
+apt-get install -y dotnet-runtime-6.0
 
 # Installing Q# and .NET quantum env
 pip3 install qsharp
 dotnet tool install -g Microsoft.Quantum.IQSharp
-/root/.dotnet/tools/dotnet-iqsharp install --user --path-to-tool="/root/.dotnet/tools/dotnet-iqsharp"
-export PATH="/root/.dotnet/tools:$PATH"
+~/.dotnet/tools/dotnet-iqsharp install --user --path-to-tool="~/.dotnet/tools/dotnet-iqsharp"
+export PATH="~/.dotnet/tools:$PATH"
 dotnet iqsharp install
