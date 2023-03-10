8b5b2c0c1526732e01ea8ed57c2c518d0251cce8
Michaël <mika89700@gmail.com>
2023-03-10 13:12:00 +0100

    Update qsharp.sh

--- build/qsharp.sh
+++ build/qsharp.sh
@@ -4,7 +4,7 @@
 wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt-get update
-apt-get install -y apt-transport-https
+apt-get install -y apt-transport-https apt-utils
 apt-get install -y dotnet-sdk-6.0
 apt-get install -y aspnetcore-runtime-6.0
 apt-get install -y dotnet-runtime-6.0
